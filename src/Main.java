380991eeb9dee8b7c4e42f3505c081d3f2375cd7
АндрейПетухов1986 <oziglena@mail.ru>
2022-12-22 22:56:01 +0300

    peremenaij2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         byte a = 58;
+        int b = 500;
+        float c = 1.5f;
 
-        System.out.println("Hello world!");
+        System.out.println("Значение переменной a с типом byte равно  " + a);
+        System.out.println("Значение переменной b с типом int равно " + b);
+        System.out.println("Значение переменной c с типом int равно " + c);
+
+        float z = (float)27.12;
+        long x = 987678965549L;
+        float s = 2.786F;
+        short d = 569;
+        short f = -159;
+        int g = 27897;
+        byte k = 67;
+
+        System.out.println(z);
+        System.out.println(x);
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(k);
+
+
+
     }
 }
